8aebb4871d8f185f731f065365796c96208825f1
Mandar Ghade <GX2H912@gmail.com>
2016-05-12 11:01:54 -0400

    Create Minecraft-Plugin.java

---
+++ src/me/FireCrystal12/Minecraft-Plugin.java
@@ -0,0 +1,28 @@
+import org.bukkit.ChatColor
+import org.bukkit.CommandSender
+import org.bukkit.JavaPlugin
+import org.bukkit.Command
+import org.bukkit.Player
+
+public class Minecraft-Plugin extends JavaPlugin{
+    
+    public void onEnable(){
+      getLogger().info("onEnable has been enabled!")
+    }
+    
+    public void onDisable(){
+    
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+      
+      if (cmd.getName().equalsIgnoreCase("mcplugin") && sender instanceof player){
+        
+        Player player = (player) sender;
+        
+        player.sendMessage(ChatColor.DARK_GREEN + "Get rekt, " + player.getName());
+        
+        
+      }
+    }
+}
